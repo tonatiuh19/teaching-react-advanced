5eabb5d9538e0c81423cba30ee5f9e1dd5c98306
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-02-22 16:16:31 -0600

    feat: creating missing pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { Router } from "@reach/router";
 import React from "react";
 import { Logo } from "./components/Logo/Logo";
 import NavBar from "./components/NavBar/NavBar";
-import { PhotoCardWithQuery } from "./containers/PhotoCardWithQuery";
 import Detail from "./pages/Detail/Detail";
+import Favs from "./pages/Favs/Favs";
 import Home from "./pages/Home/Home";
+import NotRegisteredUser from "./pages/NotRegisteredUser/NotRegisteredUser";
+import User from "./pages/User/User";
 import { GlobalStyle } from "./styles/GlobalStyle";
+
+const isAuth = true;
 
 const App = () => {
   return (
@@ -16,6 +20,17 @@ const App = () => {
         <Home path="/" />
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
+        {isAuth ? (
+          <>
+            <Favs path="/favs" />
+            <User path="/user" />
+          </>
+        ) : (
+          <>
+            <NotRegisteredUser path="/favs" />
+            <NotRegisteredUser path="/user" />
+          </>
+        )}
       </Router>
       <NavBar />
     </div>
---
+++ src/pages/Favs/Favs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Favs = () => {
+  return <div>Favs</div>;
+};
+
+export default Favs;
