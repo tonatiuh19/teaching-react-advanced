3818d6010db271b1fba1aa0d814d32c98cd8369c
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-02-22 21:18:01 -0600

    feat: using PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   output: {
@@ -8,6 +11,45 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Petagram",
+      shortname: "Pet",
+      description: "Beautiful animals",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new WorkboxPlugin.GenerateSW({
+      swDest: "service-worker.js",
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp(
+            "https://petgram-server-tonatiuh19.vercel.app"
+          ),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
