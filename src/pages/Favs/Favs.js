bf054d9018114a1bd85d8391114b1b19d22707c9
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2022-02-22 20:13:11 -0600

    feat: using Helmet

--- src/pages/Favs/Favs.js
+++ src/pages/Favs/Favs.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from "react";
 import { RenderProp } from "../../containers/GetFavorites";
+import { Helmet } from "react-helmet";
 
 const Favs = () => {
   return (
     <Fragment>
+      <Helmet>
+        <title>Petagram - Favoritos</title>
+        <meta name="description" content="Best app" />
+      </Helmet>
       <h2>Favs</h2>
       <RenderProp />
     </Fragment>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
 import { ListOfCategories } from "../../components/ListOfCategories";
 import { ListOfPhotoCards } from "../../components/ListOfPhotoCards";
+import { Helmet } from "react-helmet";
 
 const Home = ({ id }) => {
   return (
     <Fragment>
+      <Helmet>
+        <title>Petagram</title>
+        <meta name="description" content="Best app" />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
     </Fragment>
